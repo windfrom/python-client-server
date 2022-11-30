2c32d1480bc0399255b8afbcccaf364d72c0353f
windfrom <119127396+windfrom@users.noreply.github.com>
2022-11-30 15:05:35 +0300

    Added post request

--- client/client.py
+++ client/client.py
@@ -1,3 +1,6 @@
 import requests
 response = requests.get("http://127.0.0.1:5000/hello")
 print(response.content.decode("utf-8") )
+
+response = requests.post("http://127.0.0.1:5000/hello")
+print(response.content.decode("utf-8") )
